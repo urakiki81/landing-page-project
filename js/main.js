f357c9e251251a8d4eeb0b8216a2878a88cbb00c
urakiki81 <44477104+urakiki81@users.noreply.github.com>
2021-01-29 09:34:35 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,21 @@
+TweenMax.from("#screen", 1, {
+    delay: 0.4,
+    opacity: 0,
+    rotation: 27,
+    x: 80, ease: Expo.easeInOut
+ });
+ TweenMax.from("#css", 1, {
+    delay: 0.2,
+    opacity: 0,
+    x: 80, ease: Expo.easeInOut
+ });
+ TweenMax.from("#html", 1, {
+    delay: 0.5,
+    opacity: 0,
+    x: -80, ease: Expo.easeInOut
+ });
+ TweenMax.from("#person", 1, {
+    delay: 0.6,
+    opacity: 0,
+    y: -80, ease: Expo.easeInOut
+ });
